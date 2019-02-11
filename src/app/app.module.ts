2c53e102d7c544a75bf4d03e09db22c50cc262b4
Ivansolarte <chaca360@gmail.com>
2019-02-11 11:31:04 -0500

    iniciar seccion
    se realizo el iniciar seccion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { PruebaService } from './servicios/prueba.service';
 import { HttpModule } from '@angular/http';
-import { LoginComponent } from './login/login/login.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { UsuarioComponent } from './dashboard/usuario/usuario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    UsuarioComponent
   ],
   imports: [
     BrowserModule,
